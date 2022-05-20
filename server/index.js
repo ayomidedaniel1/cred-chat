220d9b7d367f87ac51d4c086eb5d59553e41d5a0
Nawed Ali <83456083+nawed2611@users.noreply.github.com>
2022-05-20 22:09:40 +0530

    Add chat UI

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const app = require('express')();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+    cors: {
+      origin: '*'}});
 
 io.on('connection', socket => {
     socket.on('message', ({ name, message }) => {
